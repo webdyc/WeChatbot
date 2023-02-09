e8a883a5ea203ed13070346a56ba4efd3c566388
董宇成 <dongyucheng@unioncast.cn>
2023-02-09 11:52:53 +0800

    部署代码

---
+++ config.js
@@ -0,0 +1,14 @@
+// 定义机器人的名称，这里是为了防止群聊消息太多，所以只有艾特机器人才会回复，
+export const botName = '小飞蛾'
+
+// 根据正则匹配是否自动通过好友验证
+export const friendShipRule = /webdyc|小飞蛾|飞蛾/
+
+// 开启会后收到ChatGPT的自动回复
+// export const autoReply = true
+
+// 群聊白名单，白名单内的群聊才会自动回复
+export const roomWhiteList = ['小飞蛾交流群']
+
+// 联系人白名单，白名单内的联系人才会自动回复
+export const aliasWhiteList = ['董宇成', '董宇博', '王澜晓', '刘影', '肖金路']
